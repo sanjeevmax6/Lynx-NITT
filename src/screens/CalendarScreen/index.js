0777c52b88899dc0f1520bc2c88be7b8dcb8693a
sanjeevmax6 <sanvegas2001@gmail.com>
2021-09-19 10:10:30 +0530

    added calendar package

--- src/screens/CalendarScreen/index.js
+++ src/screens/CalendarScreen/index.js
@@ -1,10 +1,61 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 
 const CalendarScreen = () => {
+  const dummy = () => {
+    console.log('Pressed');
+  };
   return (
     <View>
-      <Text> Calendar screen</Text>
+      <Calendar
+        // Initially visible month. Default = Date()
+        current={'2012-03-01'}
+        // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+        minDate={'2012-05-10'}
+        // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+        maxDate={'2012-05-30'}
+        // Handler which gets executed on day press. Default = undefined
+        onDayPress={() => dummy()}
+        // Handler which gets executed on day long press. Default = undefined
+        onDayLongPress={() => dummy()}
+        // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+        monthFormat={'yyyy MM'}
+        // Handler which gets executed when visible month changes in calendar. Default = undefined
+        onMonthChange={() => dummy()}
+        // Hide month navigation arrows. Default = false
+        hideArrows={true}
+        // Replace default arrows with custom ones (direction can be 'left' or 'right')
+        // renderArrow={}
+        // Do not show days of other months in month page. Default = false
+        hideExtraDays={true}
+        // If hideArrows = false and hideExtraDays = false do not switch month when tapping on greyed out
+        // day from another month that is visible in calendar page. Default = false
+        disableMonthChange={true}
+        // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
+        firstDay={1}
+        // Hide day names. Default = false
+        hideDayNames={true}
+        // Show week numbers to the left. Default = false
+        showWeekNumbers={true}
+        // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+        onPressArrowLeft={() => dummy()}
+        // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+        onPressArrowRight={() => dummy()}
+        // Disable left arrow. Default = false
+        disableArrowLeft={true}
+        // Disable right arrow. Default = false
+        disableArrowRight={true}
+        // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
+        disableAllTouchEventsForDisabledDays={true}
+        // Replace default month and year title with custom one. the function receive a date as parameter
+        renderHeader={date => {
+          /*Return JSX*/
+          dummy();
+        }}
+        // Enable the option to swipe between months. Default = false
+        enableSwipeMonths={true}
+      />
     </View>
   );
 };
--- src/screens/LoginScreen/index.js
+++ src/screens/LoginScreen/index.js
@@ -1,12 +1,90 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, SafeAreaView, ScrollView} from 'react-native';
+import {TextInput} from 'react-native-paper';
+import {scale, verticalScale, moderateScale} from 'react-native-size-matters';
+import {Button} from 'react-native-paper';
 
-const LoginScreen = () => {
+import FontAwesome, {
+  SolidIcons,
+  RegularIcons,
+  BrandIcons,
+  parseIconFromClassName,
+} from 'react-native-fontawesome';
+
+const LoginScreen = ({props}) => {
+  const [user, setUser] = useState();
+  const [pass, setPass] = useState();
+  const [passwordToggle, setaPasswordToggle] = useState(true);
+
+  const dummy = () => {
+    console.log('Icon toggled');
+  };
   return (
-    <View>
-      <Text> Login Screen </Text>
+    <View style={styles.container}>
+      <SafeAreaView>
+        <ScrollView keyboardShouldPersistTaps="always">
+          <View style={styles.textInput}>
+            <TextInput
+              label="UserName"
+              placeholder="Enter your username"
+              mode="outlined"
+              value={user}
+              onChangeText={user => setUser(user)}
+            />
+          </View>
+          <View style={styles.textInput}>
+            <TextInput
+              label="Roll Number"
+              placeholder="Enter your Roll Number"
+              mode="outlined"
+              secureTextEntry={passwordToggle}
+              right={
+                <TextInput.Icon
+                  name={
+                    <FontAwesome
+                      style={{color: '#000000'}}
+                      icon={parseIconFromClassName('fa-eye')}
+                    />
+                  }
+                  onPress={() => setaPasswordToggle(!passwordToggle)}
+                />
+              }
+              value={pass}
+              onChangeText={pass => setPass(pass)}
+            />
+          </View>
+          <View style={styles.loginButton}>
+            <Button
+              icon="camera"
+              mode="outlined"
+              loading={false}
+              icon={<FontAwesome icon={SolidIcons.smile} />}
+              onPress={() => console.log(this.props.route.params.userToken)}>
+              Login
+            </Button>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  textInput: {
+    paddingHorizontal: moderateScale(20),
+    paddingVertical: moderateScale(10),
+  },
+  loginButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    // height: verticalScale(70),
+    // width: scale(10),
+    paddingHorizontal: moderateScale(20),
+  },
+});
 
 export default LoginScreen;
