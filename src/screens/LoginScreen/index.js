652761ed64ceb4eb940a4a3aaa5e1b053eb7fa08
ANKUR SINHA <ankursinha03@gmail.com>
2021-10-30 02:34:39 +0530

    Upgrade: Login

--- src/screens/LoginScreen/index.js
+++ src/screens/LoginScreen/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -6,10 +6,9 @@ import {
   SafeAreaView,
   Dimensions,
   ScrollView,
-  TouchableHighlight,
   TouchableOpacity,
 } from 'react-native';
-import {TextInput, Button, Divider} from 'react-native-paper';
+import {TextInput} from 'react-native-paper';
 import {
   scale,
   verticalScale,
@@ -19,59 +18,17 @@ import {
 import {updateToken} from '../../redux/reducers/loginScreen';
 import {useDispatch, useSelector} from 'react-redux';
 import * as colors from '../../utils/colors';
-
 import store from '../../redux/store';
-
-const WIDTH = Dimensions.get('window').width;
+import LottieView from 'lottie-react-native';
+import lottieFile from '../../res/lottieFiles/loginBackGround.json';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const LoginScreen = ({navigation}) => {
   const [user, setUser] = useState();
   const [password, setPassword] = useState();
   const [eyeIcon, setEyeIcon] = useState('eye-off');
   const [passwordToggle, setPasswordToggle] = useState(true);
-  // const imageHeight = useRef(new Animated.Value(verticalScale(150))).current;
-  // const marginHt = useRef(new Animated.Value(verticalScale(27))).current;
   const [temp, setTemp] = useState();
-  const [student, setStudent] = useState(true);
-
-  // useEffect(() => {
-  //   const showSubscription = Keyboard.addListener(
-  //     'keyboardDidShow',
-  //     keyboardWillShow,
-  //   );
-  //   const hideSubscription = Keyboard.addListener(
-  //     'keyboardDidHide',
-  //     keyboardWillHide,
-  //   );
-
-  //   return () => {
-  //     showSubscription.remove();
-  //     hideSubscription.remove();
-  //   };
-  // }, []);
-
-  // const keyboardWillShow = event => {
-  //   Animated.timing(imageHeight, {
-  //     useNativeDriver: false,
-  //     toValue: verticalScale(100),
-  //   }).start();
-  //   Animated.timing(marginHt, {
-  //     toValue: verticalScale(7),
-  //     useNativeDriver: false,
-  //   }).start();
-  // };
-
-  // const keyboardWillHide = event => {
-  //   Animated.timing(imageHeight, {
-  //     toValue: verticalScale(150),
-  //     useNativeDriver: false,
-  //   }).start();
-
-  //   Animated.timing(marginHt, {
-  //     toValue: verticalScale(27),
-  //     useNativeDriver: false,
-  //   }).start();
-  // };
 
   const dispatch = useDispatch();
 
@@ -85,119 +42,100 @@ const LoginScreen = ({navigation}) => {
     // console.log(temp);
   };
 
-  const toggle = bool => {
-    setStudent(bool);
-  };
-
   return (
     <View style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ScrollView>
-          <View style={styles.headertextcontainer}>
-            <Text style={styles.logintext}>Login</Text>
-          </View>
+      <LottieView
+        style={{
+          marginTop: verticalScale(160),
+          backgroundColor: 'white',
+        }}
+        resizeMode="cover"
+        source={lottieFile}
+        progress={1}
+        autoPlay
+        loop
+      />
+      <View>
+        <SafeAreaView>
+          <ScrollView keyboardShouldPersistTaps="always">
+            <View style={styles.headertextcontainer}>
+              <Text style={styles.logintext}>Login</Text>
+            </View>
 
-          <View style={[styles.inputLayout, {marginBottom: verticalScale(55)}]}>
-            <View style={styles.profileImgView}>
-              <Animated.Image
-                style={{
-                  width: moderateScale(170),
-                  height: moderateScale(170),
-                  marginTop: verticalScale(70),
-                  marginBottom: verticalScale(20),
-                }}
-                source={require('../../assests/images/nitt_logo.png')}
-              />
-            </View>
-            <View style={styles.toggle}>
-              {/* <TouchableOpacity onPress={toggle(true)}> */}
-              <Text
-                style={{
-                  ...styles.toggleTextL,
-                  backgroundColor: student ? '#0080FF' : colors.Secondary,
-                }}>
-                Student
-              </Text>
-              {/* </TouchableOpacity> */}
-              <Divider style={{width: 2, backgroundColor: 'grey', flex: 1}} />
-              {/* <TouchableOpacity onPress={toggle(false)}> */}
-              <Text
-                style={{
-                  ...styles.toggleTextR,
-                  backgroundColor: student ? colors.Secondary : '#0080FF',
-                }}>
-                Club
-              </Text>
-              {/* </TouchableOpacity> */}
-            </View>
-            <View style={styles.textInput}>
-              <TextInput
-                label="Username"
-                placeholder="Enter your username"
-                mode="outlined"
-                value={user}
-                theme={{
-                  colors: {
-                    primary: 'black',
-                  },
-                }}
-                onChangeText={user => {
-                  setUser(user);
-                }}
-              />
-            </View>
-            <View style={styles.textInput}>
-              <TextInput
-                label="Password"
-                placeholder="Enter your password"
-                mode="outlined"
-                secureTextEntry={passwordToggle}
-                theme={{
-                  colors: {
-                    primary: 'black',
-                  },
-                }}
-                right={
-                  <TextInput.Icon
-                    name={eyeIcon}
-                    onPress={() => {
-                      setPasswordToggle(!passwordToggle);
-                      setEyeIcon(eyeIcon === 'eye' ? 'eye-off' : 'eye');
+            <View style={{marginBottom: verticalScale(55)}}>
+              <View style={styles.textInput}>
+                <TextInput
+                  label="Username"
+                  placeholder="Enter your username"
+                  mode="outlined"
+                  value={user}
+                  style={{backgroundColor: 'white'}}
+                  theme={{
+                    colors: {
+                      primary: 'black',
+                    },
+                  }}
+                  onChangeText={user => {
+                    setUser(user);
+                  }}
+                />
+              </View>
+              <View style={styles.textInput}>
+                <TextInput
+                  autoCorrect={false}
+                  label="Password"
+                  placeholder="Enter your password"
+                  style={{backgroundColor: 'white'}}
+                  mode="outlined"
+                  autoComplete={'off'}
+                  secureTextEntry={passwordToggle}
+                  theme={{
+                    colors: {
+                      primary: 'black',
+                    },
+                  }}
+                  right={
+                    <TextInput.Icon
+                      name={eyeIcon}
+                      onPress={() => {
+                        setPasswordToggle(!passwordToggle);
+                        setEyeIcon(eyeIcon === 'eye' ? 'eye-off' : 'eye');
+                      }}
+                    />
+                  }
+                  value={password}
+                  onChangeText={password => setPassword(password)}
+                />
+              </View>
+              <Animated.View style={styles.loginBtnView}>
+                <TouchableOpacity
+                  onPress={() => {
+                    onLogin(user);
+                  }}>
+                  <Icon
+                    name="chevron-right"
+                    size={verticalScale(44)}
+                    color={colors.WHITE}
+                    style={{
+                      backgroundColor: '#2F3F9E',
+                      borderRadius: verticalScale(22),
                     }}
                   />
-                }
-                value={password}
-                onChangeText={password => setPassword(password)}
-              />
+                </TouchableOpacity>
+              </Animated.View>
             </View>
-            <Animated.View style={styles.loginBtnView}>
-              <Button
-                style={styles.loginButton}
-                mode="outlined"
-                loading={false}
-                color="#3e863e"
-                onPress={() => {
-                  onLogin(user);
-                }}>
-                Login
-              </Button>
-            </Animated.View>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+          </ScrollView>
+        </SafeAreaView>
+      </View>
     </View>
   );
 };
 
 const styles = ScaledSheet.create({
   container: {
-    flex: 1,
-  },
-  safeArea: {
     backgroundColor: 'white',
-    height: '100%',
-    flexDirection: 'column',
-    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
   headertextcontainer: {
     flexDirection: 'row',
@@ -213,57 +151,16 @@ const styles = ScaledSheet.create({
     color: 'black',
     marginLeft: '15@s',
     alignContent: 'flex-start',
-    textDecorationLine: 'underline',
-  },
-  toggle: {
-    flexDirection: 'row',
-    alignSelf: 'center',
-    width: WIDTH / 2,
-    marginBottom: verticalScale(20),
-  },
-  toggleTextL: {
-    fontSize: scale(14),
-    padding: '5@ms',
-    width: WIDTH / 4,
-    borderColor: colors.GRAY_DARK,
-    borderWidth: 1,
-    borderTopLeftRadius: 7,
-    borderBottomLeftRadius: 7,
-    borderRightWidth: 0,
-    textAlign: 'center',
-  },
-  toggleTextR: {
-    fontSize: scale(14),
-    padding: '5@ms',
-    width: WIDTH / 4,
-    borderColor: colors.GRAY_DARK,
-    borderWidth: 1,
-    borderTopRightRadius: 7,
-    borderBottomRightRadius: 7,
-    textAlign: 'center',
-  },
-  inputLayout: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  profileImgView: {
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   textInput: {
     paddingHorizontal: moderateScale(20),
     paddingVertical: moderateScale(10),
   },
   loginBtnView: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    // height: verticalScale(70),
-    // width: scale(10),
-    marginTop: '30@vs',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    marginTop: '15@vs',
     paddingHorizontal: moderateScale(20),
-  },
-  loginButton: {
-    width: '100%',
   },
 });
 
