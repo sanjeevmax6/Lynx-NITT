8bd5b157ffb4b6a1a2adc149b6e3a7f876656a33
ANKUR SINHA <ankursinha03@gmail.com>
2021-12-28 14:56:23 +0530

    Added: Success Screen

---
+++ src/components/SuccessScreen/index.js
@@ -0,0 +1,101 @@
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import LottieView from 'lottie-react-native';
+import successLottie from '../../res/lottieFiles/success.json';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {scale, verticalScale} from 'react-native-size-matters';
+import {ICON_SIZE_LARGE} from '../../utils/UI_CONSTANTS';
+
+import * as colors from '../../utils/colors';
+
+const SuccessScreen = ({
+  showButton = true,
+
+  buttonText = 'NEXT',
+  showIconInButton = true,
+  icon = 'keyboard-arrow-right',
+  fn = () => {},
+}) => {
+  const backPress = () => {
+    fn();
+  };
+
+  const [STATE, setSTATE] = useState(1);
+  const toggler = () => {
+    //force reload as there is a bug in the LF library
+    if (STATE) setSTATE(0);
+  };
+
+  setTimeout(toggler, 50);
+
+  return (
+    <View
+      style={{
+        height: '100%',
+        width: '100%',
+        alignSelf: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+      }}>
+      <LottieView
+        style={{marginBottom: verticalScale(50)}}
+        source={successLottie}
+        speed={0.7}
+        resizeMode="contain"
+        autoPlay
+        loop={false}
+      />
+
+      {showButton ? (
+        <View
+          style={{
+            padding: scale(9),
+            paddingLeft: scale(18),
+            borderRadius: scale(24),
+            alignSelf: 'center',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '50%',
+            position: 'absolute',
+            backgroundColor: colors.Accent,
+            bottom: verticalScale(50),
+          }}>
+          <TouchableOpacity
+            style={{
+              flexDirection: 'row',
+              borderRadius: scale(24),
+              alignItems: 'center',
+            }}
+            onPress={backPress}>
+            <Text
+              style={{
+                fontSize: scale(18),
+                color: 'white',
+
+                fontWeight: 'bold',
+              }}>
+              {buttonText}
+            </Text>
+            {showIconInButton ? (
+              <Icon
+                name={icon}
+                color={colors.Primary}
+                size={scale(ICON_SIZE_LARGE)}
+                style={{marginLeft: scale(6)}}
+              />
+            ) : (
+              <View style={{width: scale(8)}} />
+            )}
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <></>
+      )}
+    </View>
+  );
+};
+
+export default SuccessScreen;
