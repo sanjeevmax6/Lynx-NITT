84434bc3568f9ede8f453c8c191cba2ad55c5af3
ANKUR SINHA <ankursinha03@gmail.com>
2022-01-06 00:43:49 +0530

    Added: Pull down to Refresh in Student User Screen

--- src/screens/StudentUserScreen/ClubFollowingScreen.js
+++ src/screens/StudentUserScreen/ClubFollowingScreen.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import {View, FlatList} from 'react-native';
+import {View, FlatList, RefreshControl} from 'react-native';
 import {verticalScale} from 'react-native-size-matters';
 import ListItem from './ClubListItem';
 import {listScreenStyles} from './styles';
+import * as colors from '../../utils/colors';
+import {getAllStudentDetails} from './apiCalls';
 
 const headerFooterComponent = () => {
   return <View style={{height: verticalScale(6)}} />;
 };
 
 const ClubFollowingScreen = ({clubFollowing, goToClub}) => {
+  const [refreshing, setRefreshing] = React.useState(false);
+  const onRefresh = () => {
+    setRefreshing(false);
+    console.log('refreshing');
+    getAllStudentDetails();
+  };
+
   return (
     <View style={listScreenStyles.container}>
       <FlatList
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            colors={[colors.Accent]}
+            onRefresh={onRefresh}
+          />
+        }
         style={listScreenStyles.listStyle}
         ListHeaderComponent={headerFooterComponent}
         ListFooterComponent={headerFooterComponent}
--- src/screens/StudentUserScreen/InterestedEventsScreen.js
+++ src/screens/StudentUserScreen/InterestedEventsScreen.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import {View, FlatList} from 'react-native';
+import {View, FlatList, RefreshControl} from 'react-native';
 import ListItem from './EventListItem';
 import {listScreenStyles} from './styles';
 import {verticalScale} from 'react-native-size-matters';
+import * as colors from '../../utils/colors';
+import {getAllStudentDetails} from './apiCalls';
+
 const headerFooterComponent = () => {
   return <View style={{height: verticalScale(6)}} />;
 };
 
 const InterestedEventsScreen = ({interestedEvents, goToEvent}) => {
+  const [refreshing, setRefreshing] = React.useState(false);
+  const onRefresh = () => {
+    setRefreshing(false);
+    console.log('refreshing');
+    getAllStudentDetails();
+  };
+
   return (
     <View style={listScreenStyles.container}>
       <FlatList
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            colors={[colors.Accent]}
+            onRefresh={onRefresh}
+          />
+        }
         style={listScreenStyles.listStyle}
         data={interestedEvents}
         ListHeaderComponent={headerFooterComponent}
